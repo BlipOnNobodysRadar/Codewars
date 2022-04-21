c3a6f5ef3f82053ff4c903af638070624ee29e82
Ryan Lowe <somebody480@gmail.com>
2022-04-20 20:29:11 -0400

    update kata1, returns array

--- 2022-04-20/kata1.js
+++ 2022-04-20/kata1.js
@@ -12,21 +12,21 @@ For example, a tower with 3 floors looks like this:
 */
 
 function towerBuilder(nFloors) {
+    let tower = [];
     let startStr = "*"
     let stringLength = (nFloors * 2) -1;
-    //string size is 1 + nFloors
-    //length is n*2 -1;
     let currentStr = startStr;
     let spacePad = "".padEnd((stringLength - 1)/2, " ");
     let outputString = spacePad + currentStr + spacePad;
-    console.log(outputString);
+    tower.push(outputString);
     for(let i = 1; i < nFloors; i++) {
         currentStr = startStr.padEnd(i*2+1,'*');
         spacePad = "".padEnd(((stringLength - 1)/2)-i, ' ');
         outputString = spacePad + currentStr + spacePad;
-        console.log(outputString);
+        tower.push(outputString);
     }
+    return tower;
 
     
-  }
-  towerBuilder(10);
+}
+  console.log(towerBuilder(10));
