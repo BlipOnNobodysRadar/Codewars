5e2872080ae903f141dd2fc1ef96d73050bb67e2
Ryan Lowe <somebody480@gmail.com>
2022-04-27 22:49:43 -0400

    daily kata4

---
+++ 2022-04-27/kata4.js
@@ -0,0 +1,36 @@
+// https://www.codewars.com/kata/555eded1ad94b00403000071/train/javascript
+
+/*
+Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+
+Rules:
+
+    You need to round the answer to 2 decimal places and return it as String.
+
+    If the given value is 0 then it should return 0.00
+
+    You will only be given Natural Numbers as arguments.
+
+Examples:(Input --> Output)
+
+1 --> 1 --> "1.00"
+2 --> 1 + 1/4 --> "1.25"
+5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+*/
+function SeriesSum(n)
+{
+  if(n === 0) return '0.00';
+  
+  let bottomVal = 4;
+  let returnVal = 1;
+  
+  if(n > 1) {
+    for(let i = 1; i < n; i++) {
+      returnVal += 1/bottomVal;
+      bottomVal += 3;
+    }
+  }
+  return returnVal.toFixed(2);
+}
