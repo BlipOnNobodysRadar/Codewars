ddcf3ca2861ac7804db18f9ec3c1b20caa11be55
Ryan Lowe <somebody480@gmail.com>
2022-04-30 15:45:59 -0400

    daily kata6

---
+++ 2022-04-30/kata6.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/57a0885cbb9944e24c00008e/train/javascript
+/*
+Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+*/
+function removeExclamationMarks(s) {
+    return s.split("!").join("");
+  }
