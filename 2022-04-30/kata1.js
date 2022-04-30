611c6196894752978d7e7cac3f070511b364bb38
Ryan Lowe <somebody480@gmail.com>
2022-04-30 15:02:48 -0400

    daily kata1 complete

---
+++ 2022-04-30/kata1.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/57eaeb9578748ff92a000009/train/javascript
+/*
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
+*/
+function sumMix(x){
+    return x.reduce((sum, val) => sum + Number(val),0);
+  }
