97dba9f45f133e15825490e307489d28c60a94dd
Ryan Lowe <somebody480@gmail.com>
2022-04-30 15:20:23 -0400

    kata3 optimize

--- 2022-04-30/kata3.js
+++ 2022-04-30/kata3.js
@@ -10,9 +10,6 @@ uniqueInOrder([1,2,2,3,3])       == [1,2,3]
 */
 var uniqueInOrder=function(iterable){
     let newArr = [];
-    if(typeof iterable === 'string') {
-      iterable = iterable.split("");
-    }
     if(iterable[0]) newArr.push(iterable[0]);
     for(let i = 1; i < iterable.length; i++) {
       if(iterable[i] !== iterable[i-1]) newArr.push(iterable[i]);
