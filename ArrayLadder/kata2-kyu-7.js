175bf68775bb15011d9e20fad930cca4e260d1e6
Ryan Lowe <somebody480@gmail.com>
2022-05-03 19:11:21 -0400

    ArrayLadder kata2

--- ArrayLadder/kata2-kyu-7.js
+++ ArrayLadder/kata2-kyu-7.js
@@ -22,3 +22,14 @@ arrAdder([
 ['t','e','e','']
 ]) // => "Just Live Life Man"
 */
+function arrAdder(arr) {
+    let words = [];
+    for(let i = 0; i < arr[0].length; i++) {
+      for(let j = 0; j < arr.length; j++) {
+        words.push(arr[j][i]); 
+      }
+      words.push(' ');
+    }
+    words.pop();
+    return words.join("");
+  }
