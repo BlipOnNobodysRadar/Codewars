e1c0bb26988f4ced366c0b05b9b338dc50c5e726
Ryan Lowe <somebody480@gmail.com>
2022-05-08 21:20:03 -0400

    complete CanWeDivideIt kata

--- 2022-05-08/AlternatingCase.js
+++ 2022-05-08/AlternatingCase.js
@@ -15,3 +15,9 @@ Define String.prototype.toAlternatingCase (or a similar function/method such as 
 
 As usual, your function/method should be pure, i.e. it should not mutate the original string.
 */
+String.prototype.toAlternatingCase = function () {
+    return this
+                .split("")
+                .map(letter => letter.toUpperCase() !== letter ? letter.toUpperCase() : letter.toLowerCase())
+                .join("");
+  }
