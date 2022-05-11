dca3fd2d1a03329c4e2f5637a79797fadb773d35
Ryan Lowe <somebody480@gmail.com>
2022-05-11 19:34:02 -0400

    arithmetic kata complete

--- 2022-05-11/getMiddle.js
+++ 2022-05-11/getMiddle.js
@@ -6,7 +6,5 @@ function getMiddle(s)
   if(sArr.length %2 === 0) {
     return (sArr[sArr.length/2-1] + sArr[sArr.length/2])
   }
-  console.log(`ArrLength ${sArr.length} IsEven ${sArr.length %2 === 0} \n
-  EvenMid: ${sArr[sArr.length/2-1] + sArr[sArr.length/2]} OddMid: ${sArr[parseInt(sArr.length/2+1)]}`)
-  return sArr[parseInt(sArr.length/2+1)];
+  return sArr[parseInt(sArr.length/2)];
 }
