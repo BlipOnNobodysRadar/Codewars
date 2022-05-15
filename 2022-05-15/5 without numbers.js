a5778a52b7ef9e0a5551ca33284f48113ae8edbe
Ryan Lowe <somebody480@gmail.com>
2022-05-15 16:03:14 -0400

    5 without numbers kata complete

---
+++ 2022-05-15/5 without numbers.js
@@ -0,0 +1,6 @@
+//https://www.codewars.com/kata/59441520102eaa25260000bf/train/javascript
+
+function unusualFive() {
+    const numbers = ['zero','one','two','three','four','five']
+    return numbers.indexOf('five');
+  }
