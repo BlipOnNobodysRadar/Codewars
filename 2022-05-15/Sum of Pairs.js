f8bce8fab1ff5b17b88b73a2586c06d319adaebf
Ryan Lowe <somebody480@gmail.com>
2022-05-15 22:56:45 -0400

    Sum of Pairs complete

--- 2022-05-15/Sum of Pairs.js
+++ 2022-05-15/Sum of Pairs.js
@@ -2,12 +2,26 @@
 
 // return first pair as an array
 function sumPairs(ints, s) {
+    const existing = new Set()
+    for(i of ints) {
+        if(existing.has(s-i)) {
+            return [s - i, i]
+        }
+        existing.add(i)
+    }
+}
+    /*
     //j + j+i where i increases by 1 each loop, inner loop (if j + i >= length -1 break)
+    //
+    for(let i = 0; i < ints.length; i++) {
+        if(ints.contains(s - ints[i]))
+    }
+
     for(let i = 1; i < ints.length; i++) {
         for(let j = 0; j < ints.length; j++) {
             if(j + i >= ints.length) break;
             if(ints[j] + ints[j+i] === s) return [ints[j],ints[j+i]];
         }
     }
-  }
+  }*/
 //works, but not efficient.
