8769da72e435844185efcb733f8f9270e7e4c425
Ryan Lowe <somebody480@gmail.com>
2022-06-16 08:40:25 -0400

    A wolf in sheep's clothing kata complete

---
+++ 6-16-2022/A wolf in sheep's clothing.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/5c8bfa44b9d1192e1ebd3d15/train/javascript
+
+function warnTheSheep(queue) {
+  return queue[queue.length - 1] === 'wolf'
+    ? 'Pls go away and stop eating my sheep'
+    : `Oi! Sheep number ${
+        queue.length - queue.indexOf('wolf') - 1
+      }! You are about to be eaten by a wolf!`;
+}
