27c95479e938cc2d02dc5c1f9df7b469bdf80e4e
Ryan Lowe <somebody480@gmail.com>
2022-06-19 15:25:52 -0400

    Conor McGregor vs George Saint Pierre kata complete

---
+++ 6-19-2022/Conor McGregor vs George Saint Pierre.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/582dafb611d576b745000b74/train/javascript
+// or UFC Fans (Total Beginners): Conor McGregor vs George Saint Pierre
+
+var quote = function (fighter) {
+  return fighter.toLowerCase() === 'george saint pierre'
+    ? 'I am not impressed by your performance.'
+    : "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+};
