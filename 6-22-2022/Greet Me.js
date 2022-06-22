1d6b386e9d27c1a3baea8aac31b124859c1d83a9
Ryan Lowe <somebody480@gmail.com>
2022-06-22 16:15:56 -0400

    Greet Me kata complete

---
+++ 6-22-2022/Greet Me.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/535474308bb336c9980006f2/train/javascript
+// Greet Me
+
+var greet = function (name) {
+  return `Hello ${name.toUpperCase()[0] + name.toLowerCase().slice(1)}!`;
+};
