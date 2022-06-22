6c0bde39ed87dd4b2b1fa1489f6e916a11be923b
Ryan Lowe <somebody480@gmail.com>
2022-06-22 15:04:32 -0400

    Refactored Dashatize kata to fit into one statement

--- 6-22-2022/Dashatize it.js
+++ 6-22-2022/Dashatize it.js
@@ -1,18 +1,17 @@
 // https://www.codewars.com/kata/58223370aef9fc03fd000071/train/javascript
 
 function dashatize(num) {
-  if (!Number.isInteger(num)) return 'NaN';
-  let returnStr = Math.abs(num)
-    .toString()
-    .split('')
-    .map((d) => (Number(d) % 2 === 0 ? d : `-${d}-`))
-    .join('');
-  if (returnStr.startsWith('-')) returnStr = returnStr.slice(1);
-  if (returnStr.endsWith('-')) returnStr = returnStr.slice(0, -1);
-  returnStr = returnStr
-    .split('-')
-    .filter((v) => v !== '')
-    .join('-');
-  return returnStr;
+  return !Number.isInteger(num)
+    ? 'NaN'
+    : Math.abs(num)
+        .toString()
+        .split('')
+        .map((d) => (Number(d) % 2 === 0 ? d : `-${d}-`))
+        .join('')
+        .split('-')
+        .filter((v) => v !== '')
+        .join('-');
 }
 console.log(dashatize(274));
+
+// Could be made more concise.
