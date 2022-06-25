2791ad752c42e2d274aa699f6873d80f583e58ba
Ryan Lowe <somebody480@gmail.com>
2022-06-24 22:54:05 -0400

    Remove the time kata complete

---
+++ 6-24-2022/Remove the time.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/56b0ff16d4aa33e5bb00008e/train/javascript
+// Remove the time
+
+function shortenToDate(longDate) {
+  return longDate.slice(0, longDate.indexOf(","));
+}
