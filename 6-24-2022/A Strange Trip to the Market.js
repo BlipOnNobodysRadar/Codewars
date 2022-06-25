58f3c909cf43468f01393524928fe848b2c9d174
Ryan Lowe <somebody480@gmail.com>
2022-06-24 22:45:56 -0400

    A Strange Trip to the Market kata complete

---
+++ 6-24-2022/A Strange Trip to the Market.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/55ccdf1512938ce3ac000056/train/javascript
+// A Strange Trip to the Market
+function isLockNessMonster(s) {
+  return (
+    s.includes("3.50") || s.includes("tree fiddy") || s.includes("three fifty")
+  );
+}
