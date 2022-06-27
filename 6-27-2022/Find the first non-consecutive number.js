772764723df492c0f7d6182e2d30e079f20ff772
Ryan Lowe <somebody480@gmail.com>
2022-06-27 17:37:29 -0400

    Find the first non-consecutive number kata complete

---
+++ 6-27-2022/Find the first non-consecutive number.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/58f8a3a27a5c28d92e000144/train/javascript
+// Find the first non-consecutive number
+function firstNonConsecutive(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] - arr[i - 1] !== 1) return arr[i];
+  }
+  return null;
+}
