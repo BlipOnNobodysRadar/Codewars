fea09cb19c6da3dc094d9567a026362e889a6e86
Ryan Lowe <somebody480@gmail.com>
2022-06-27 17:46:08 -0400

    The Vowel Code kata complete

---
+++ 6-27-2022/The Vowel Code.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/53697be005f803751e0015aa/train/javascript
+// The Vowel Code
+
+function encode(string) {
+  const vSwap = { a: 1, e: 2, i: 3, o: 4, u: 5 };
+  return string.split("").map((d) => (vSwap[d] ? vSwap[d] : d));
+}
+
+function decode(string) {
+  const vSwap = { 1: "a", 2: "e", 3: "i", 4: "o", 5: "o" };
+  return string.split("").map((d) => (vSwap[d] ? vSwap[d] : d));
+}
