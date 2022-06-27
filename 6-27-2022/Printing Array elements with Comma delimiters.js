7ec05cb31499b08991c065e8ad04c110f5cdc18c
Ryan Lowe <somebody480@gmail.com>
2022-06-27 17:21:21 -0400

    Printing Array elements with Comma delimiters kata complete

---
+++ 6-27-2022/Printing Array elements with Comma delimiters.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/56e2f59fb2ed128081001328/train/javascript
+// Printing Array elements with Comma delimiters
+
+function printArray(array) {
+  return array.join(",");
+}
