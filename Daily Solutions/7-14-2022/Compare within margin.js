92395df1b4ae3ef6a8aa5e96f21624af710ca9b1
Ryan Lowe <somebody480@gmail.com>
2022-07-14 12:51:38 -0400

    Compare with margin kata complete

---
+++ Daily Solutions/7-14-2022/Compare within margin.js
@@ -0,0 +1,5 @@
+// Compare within margin
+// https://www.codewars.com/kata/56453a12fcee9a6c4700009c/train/javascript
+function closeCompare(a, b, margin = 0) {
+  return Math.abs(a - b) <= margin ? 0 : a - b > 0 ? 1 : -1;
+}
