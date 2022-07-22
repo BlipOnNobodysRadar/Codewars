c8f223c89b6eb8a92a776384f4574d554f517d08
Ryan Lowe <somebody480@gmail.com>
2022-07-22 12:18:09 -0400

    Duck Duck Goose (8kyu)

---
+++ Daily Solutions/7-22-2022/Duck Duck Goose (8kyu).js
@@ -0,0 +1,6 @@
+// Duck Duck Goose (8kyu)
+// https://www.codewars.com/kata/582e0e592029ea10530009ce/train/javascript
+
+function duckDuckGoose(players, goose) {
+  return players[(goose - 1) % players.length].name;
+}
