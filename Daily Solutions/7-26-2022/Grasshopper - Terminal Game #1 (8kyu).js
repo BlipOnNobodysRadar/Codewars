311243f88d5eccf9742c1760dd5ae3195dcefc5d
Ryan Lowe <somebody480@gmail.com>
2022-07-26 13:52:26 -0400

    Grasshopper - Terminal Game #1 (8kyu) complete

---
+++ Daily Solutions/7-26-2022/Grasshopper - Terminal Game #1 (8kyu).js
@@ -0,0 +1,10 @@
+// Grasshopper - Terminal Game #1 (8kyu)
+// https://www.codewars.com/kata/55e8aba23d399a59500000ce/train/javascript
+
+function Hero(name = "Hero") {
+  this.name = name;
+  this.position = "00";
+  this.health = 100;
+  this.damage = 5;
+  this.experience = 0;
+}
