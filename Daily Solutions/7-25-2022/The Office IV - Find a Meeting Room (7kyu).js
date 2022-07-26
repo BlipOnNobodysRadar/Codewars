3069f91909b4832086f241267bab2c2b3fbb5c95
Ryan Lowe <somebody480@gmail.com>
2022-07-25 20:02:22 -0400

    The Office IV - Find a Meeting Room (7kyu) complete

---
+++ Daily Solutions/7-25-2022/The Office IV - Find a Meeting Room (7kyu).js
@@ -0,0 +1,6 @@
+// The Office IV - Find a Meeting Room (7kyu)
+// https://www.codewars.com/kata/57f604a21bd4fe771b00009c/train/javascript
+
+function meeting(x) {
+  return x.indexOf("O") === -1 ? "None available!" : x.indexOf("O");
+}
