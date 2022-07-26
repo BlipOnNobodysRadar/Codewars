6ee304c9c3ab89d7a9bcaab47e8726cdc4a614a5
Ryan Lowe <somebody480@gmail.com>
2022-07-26 14:13:30 -0400

    Data Reverse (6kyu) complete

--- Daily Solutions/7-26-2022/Data Reverse (6kyu).js
+++ Daily Solutions/7-26-2022/Data Reverse (6kyu).js
@@ -1,17 +1,13 @@
-// Data Reverse
+// Data Reverse (6kyu)
 // https://www.codewars.com/kata/569d488d61b812a0f7000015/train/javascript
 
 function dataReverse(data) {
   // break into subseq's of 8, reverse the order of that, join
-  //console.log(data.splice(0,8));
-
-  //O(n) -- just loop from the end of the data array and construct return arr as you go.
   const seqs = [];
   while (data.length > 0) {
-    // O(n/8)
     seqs.push(data.splice(0, 8));
   }
-  seqs.reverse(); // unsure of O
+  seqs.reverse();
   let retArr = [];
   seqs.forEach((seq) => {
     retArr = retArr.concat(seq);
