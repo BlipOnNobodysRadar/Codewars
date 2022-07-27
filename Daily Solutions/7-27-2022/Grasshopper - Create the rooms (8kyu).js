7a9e2e6535228bb8d0501a4e99bcc2f446388020
Ryan Lowe <somebody480@gmail.com>
2022-07-27 12:12:34 -0400

    Grasshopper - Create the rooms (8kyu) complete

--- Daily Solutions/7-27-2022/Grasshopper - Create the rooms (8kyu).js
+++ Daily Solutions/7-27-2022/Grasshopper - Create the rooms (8kyu).js
@@ -1,2 +1,16 @@
 // Grasshopper - Create the rooms
 // https://www.codewars.com/kata/56a29b237e9e997ff2000048/train/javascript
+
+// Add rooms here
+class Room {
+  constructor(name = "", description = "", completed = false) {
+    this.name = name;
+    this.description = description;
+    this.completed = completed;
+  }
+}
+var rooms = {
+  room1: new Room("room1", "yep"),
+  room2: new Room(),
+  room3: new Room("room3", "nope"),
+};
