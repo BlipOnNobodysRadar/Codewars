51e5820df8fc7f04e7cdab99a6ed139f39e5e27b
Ryan Lowe <somebody480@gmail.com>
2022-07-31 19:29:33 -0400

    Do I get a bonus (8kyu)

---
+++ Daily Solutions/7-31-2022/Do I get a bonus (8kyu).js
@@ -0,0 +1,6 @@
+// Do I get a bonus (8kyu)
+// https://www.codewars.com/kata/56f6ad906b88de513f000d96/train/javascript
+
+function bonusTime(salary, bonus) {
+  return bonus ? `£${salary * 10}` : `£${salary}`;
+}
