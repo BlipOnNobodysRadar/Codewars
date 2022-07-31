54e9f332e53b65074f52efad7945d1e211a51ae9
Ryan Lowe <somebody480@gmail.com>
2022-07-31 19:27:40 -0400

    Count the Monkeys! (8kyu)

---
+++ Daily Solutions/7-31-2022/Count the Monkeys! (8kyu).js
@@ -0,0 +1,10 @@
+// Count the Monkeys! (8kyu)
+// https://www.codewars.com/kata/56f69d9f9400f508fb000ba7/train/javascript
+
+function monkeyCount(n) {
+  const arr = [];
+  for (let i = 1; i <= n; i++) {
+    arr.push(i);
+  }
+  return arr;
+}
