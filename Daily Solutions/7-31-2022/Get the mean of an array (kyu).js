8ba09f1cf22e6a3a7faf5eea54c183050de168ae
Ryan Lowe <somebody480@gmail.com>
2022-07-31 19:24:31 -0400

    Get the mean of an array (8kyu)

---
+++ Daily Solutions/7-31-2022/Get the mean of an array (kyu).js
@@ -0,0 +1,5 @@
+// Get the mean of an array (kyu)
+// https://www.codewars.com/kata/563e320cee5dddcf77000158/train/javascript
+function getAverage(marks) {
+  return parseInt(marks.reduce((sum, m) => sum + m, 0) / marks.length);
+}
