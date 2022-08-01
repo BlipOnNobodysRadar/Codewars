0a0bf6d9fcef8419c0f36de3307b38cb91f6ea1e
Ryan Lowe <somebody480@gmail.com>
2022-07-31 20:40:20 -0400

    8kyu done

---
+++ Daily Solutions/7-31-2022/Generate range of integers (8kyu).js
@@ -0,0 +1,9 @@
+// Generate range of integers (8kyu)
+
+function generateRange(min, max, step) {
+  let arr = [];
+  for (let i = min; i <= max; i += step) {
+    arr.push(i);
+  }
+  return arr;
+}
