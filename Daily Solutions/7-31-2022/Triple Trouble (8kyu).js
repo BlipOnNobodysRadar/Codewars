d1131d64d6a15d4736be3ce452db5c5b000f1a7a
Ryan Lowe <somebody480@gmail.com>
2022-07-31 20:48:41 -0400

    8kyu done

---
+++ Daily Solutions/7-31-2022/Triple Trouble (8kyu).js
@@ -0,0 +1,9 @@
+// Triple Trouble (8kyu)
+
+function tripleTrouble(one, two, three) {
+  let str = "";
+  for (let i = 0; i < one.length; i++) {
+    str += one[i] + two[i] + three[i];
+  }
+  return str;
+}
