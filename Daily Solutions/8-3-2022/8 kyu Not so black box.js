132c32e8fd2690ab1715ee71e513858ccbc96608
Ryan Lowe <somebody480@gmail.com>
2022-08-03 19:08:26 -0400

    comment tweak

--- Daily Solutions/8-3-2022/8 kyu Not so black box.js
+++ Daily Solutions/8-3-2022/8 kyu Not so black box.js
@@ -3,3 +3,5 @@ blackBox();
 JSopenSesame();
 
 // The solution is to console.log(blackBox.toString()) to see the source code.
+// If you're curious, you can do the same to JSopenSesame() and read its code
+// to solve it without calling JSopenSesame()
