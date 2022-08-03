cf46ec057836d4fd9d92d9871583c698618bf420
Ryan Lowe <somebody480@gmail.com>
2022-08-03 18:52:07 -0400

    tweak

--- Daily Solutions/8-3-2022/8 kyu noobCode 02 TRICKY QUESTIONS ( primitives and operator precedence).js
+++ Daily Solutions/8-3-2022/8 kyu noobCode 02 TRICKY QUESTIONS ( primitives and operator precedence).js
@@ -1,4 +1,6 @@
 // 8 kyu noobCode 02: TRICKY QUESTIONS ( primitives and operator precedence)
 function greaterThanLessThan(a, b, c) {
   return a < b < c;
+  // (a < b) < c
+  // prettier automatically removes () but they are needed.
 }
