277b6f860212f5ae9cb220a69cb151a4ded9ddb9
Ryan Lowe <somebody480@gmail.com>
2022-08-04 10:53:44 -0400

    8 kyu What's up next

---
+++ Daily Solutions/8-4-2022/8 kyu What's up next.js
@@ -0,0 +1,10 @@
+// 8 kyu What's up next
+function nextItem(xs, item) {
+  let found = undefined;
+  // for of loops iterate over any iterable object, including arrays.
+  for (let x of xs) {
+    if (found) return x; // found set in previous iteration
+    if (x == item) found = true; // executes if item found
+  }
+  return found;
+}
