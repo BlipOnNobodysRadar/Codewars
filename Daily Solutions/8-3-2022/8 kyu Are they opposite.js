08709bed8167e7baa7926d46372e18208839dfeb
Ryan Lowe <somebody480@gmail.com>
2022-08-03 22:43:23 -0400

    fix2

--- Daily Solutions/8-3-2022/8 kyu Are they opposite.js
+++ Daily Solutions/8-3-2022/8 kyu Are they opposite.js
@@ -2,7 +2,7 @@
 function isOpposite(s1, s2) {
   if (s1 === s2 || s1.length !== s2.length) return false;
   for (let i = 0; i < s1.length; i++) {
-    if (s1[i] === s2[i]) {
+    if (s1[i] === s2[i] || s1[i].toLowerCase() !== s2[i].toLowerCase()) {
       return false;
     }
   }
