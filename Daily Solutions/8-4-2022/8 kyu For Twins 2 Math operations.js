094d64855099336a91b4c8c7304f7db732f47089
Ryan Lowe <somebody480@gmail.com>
2022-08-04 10:41:28 -0400

    8 kyu For Twins 2 Math operations

---
+++ Daily Solutions/8-4-2022/8 kyu For Twins 2 Math operations.js
@@ -0,0 +1,6 @@
+// 8 kyu For Twins 2 Math operations.js
+function iceBrickVolume(radius, bottleLength, rimLength) {
+  return (bottleLength - rimLength) * 2 * radius ** 2;
+}
+
+// Hint: the area of a square inside of a circle is 2*(r^2)
