f04b5d48cbd62214e19ca0d5a2955348cdbd607f
Ryan Lowe <somebody480@gmail.com>
2022-08-04 11:19:46 -0400

    commented code

--- Daily Solutions/8-4-2022/8 kyu Count words.js
+++ Daily Solutions/8-4-2022/8 kyu Count words.js
@@ -3,12 +3,12 @@ function countWords(str) {
   str = str.toLowerCase();
   // establish valid non-deliminator characters, lowercase only for ease.
   const nonDelim = "abcdefghijklmnopqrstuvwxyz-'`0123456789".split("");
-  // deliminates the string
+  // Partially deliminates the string by converting non-delim chars into whitespace
   let deliminated = str
     .split("")
     .map((c) => (nonDelim.includes(c) ? c : " "))
     .join("");
-  // cleans the deliminated string of any "empty" (whitespace only) entries
+  // Completes delimination process by "cleaning" the string of any "empty" (whitespace only) entries
   let cleaned = deliminated.split(" ").filter((s) => s.trim() !== "");
   return cleaned.length;
 }
