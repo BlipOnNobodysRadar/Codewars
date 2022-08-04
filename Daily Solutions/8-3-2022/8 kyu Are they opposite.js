64c54206a11323d8bd4536b3db9e2218775da3f9
Ryan Lowe <somebody480@gmail.com>
2022-08-03 22:42:06 -0400

    fix

--- Daily Solutions/8-3-2022/8 kyu Are they opposite.js
+++ Daily Solutions/8-3-2022/8 kyu Are they opposite.js
@@ -1,4 +1,4 @@
-// 8 kyu They say that only the name is long enough to attract attention. They also said that only a simple Kata will have someone to solve it. This is a sadly story #1: Are they opposite?
+// 8 kyu Are they opposite?
 function isOpposite(s1, s2) {
   if (s1 === s2 || s1.length !== s2.length) return false;
   for (let i = 0; i < s1.length; i++) {
