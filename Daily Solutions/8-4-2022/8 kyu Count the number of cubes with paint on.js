4b6c8fa6cd061c6fe8b8ab576280323c54d15202
Ryan Lowe <somebody480@gmail.com>
2022-08-04 11:50:59 -0400

    8 kyu Count the number of cubes with paint on

--- Daily Solutions/8-4-2022/8 kyu Count the number of cubes with paint on.js
+++ Daily Solutions/8-4-2022/8 kyu Count the number of cubes with paint on.js
@@ -1,4 +1,8 @@
 // 8 kyu Count the number of cubes with paint on
 var countSquares = function (cuts) {
-  return 6 * cuts ** 2 + 2;
+  return cuts === 0 ? 1 : 6 * cuts ** 2 + 2;
 };
+
+// Solution found by coming up with first few solutions mentally:
+// 0 = 1, 1 = 8, 2 = 26... and using that sequence to lookup formula.
+// I'm not about to reinvent math.
