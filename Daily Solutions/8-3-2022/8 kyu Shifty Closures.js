efae59495477903319481c5d85298813c516ea07
Ryan Lowe <somebody480@gmail.com>
2022-08-03 21:53:27 -0400

    8 kyu Shifty Closures

--- Daily Solutions/8-3-2022/8 kyu Shifty Closures.js
+++ Daily Solutions/8-3-2022/8 kyu Shifty Closures.js
@@ -3,7 +3,8 @@ var name = "Abe";
 var greet_abe = function () {
   return "Hello, " + name + "!";
 };
-name = "Ben";
+
 var greet_ben = function () {
+  name = "Ben";
   return "Hello, " + name + "!";
 };
