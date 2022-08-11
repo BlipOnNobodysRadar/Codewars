18cfb52c08619a4ed137418ff1901989cb369428
Ryan Lowe <somebody480@gmail.com>
2022-08-11 09:14:50 -0400

    commented code

--- Daily Solutions/8-11-2022/7 kyu My Language Skills.js
+++ Daily Solutions/8-11-2022/7 kyu My Language Skills.js
@@ -2,7 +2,7 @@
 function myLanguages(results) {
   let arr = [];
   for (const key in results) {
-    if (results[key] >= 60) arr.push([results[key], key]);
+    if (results[key] >= 60) arr.push([results[key], key]); // pushes result paired with key if above 60
   }
-  return arr.sort((a, b) => b[0] - a[0]).map((pair) => pair[1]);
+  return arr.sort((a, b) => b[0] - a[0]).map((pair) => pair[1]); //sorts in descending order based on grade, then maps to only the keys
 }
