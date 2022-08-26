9205c4276fd9b9a070665b0701b474845f856592
Ryan Lowe <somebody480@gmail.com>
2022-08-26 13:59:59 -0400

    7 kyu String Merge.js

---
+++ Daily Solutions/8-26-2022/7kyu String Merge.js
@@ -0,0 +1,7 @@
+// 7 kyu String Merge.js
+function stringMerge(string1, string2, letter) {
+  return (
+    string1.slice(0, string1.indexOf(letter)) +
+    string2.slice(string2.indexOf(letter))
+  );
+}
