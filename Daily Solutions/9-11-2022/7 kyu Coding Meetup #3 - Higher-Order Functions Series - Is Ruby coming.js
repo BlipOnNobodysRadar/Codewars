d2bdad14b4367eea229b9ea7fff2f1815b02667e
Ryan Lowe <somebody480@gmail.com>
2022-09-14 09:41:38 -0400

    pushing files I forgot to save

---
+++ Daily Solutions/9-11-2022/7 kyu Coding Meetup #3 - Higher-Order Functions Series - Is Ruby coming.js
@@ -0,0 +1,4 @@
+// 7 kyu Coding Meetup #3 - Higher-Order Functions Series - Is Ruby coming?
+function isRubyComing(list) {
+  return list.some((obj) => obj.language === "Ruby");
+}
--- Daily Solutions/9-8-2022/7 kyu Coding Meetup #2 - Higher-Order Functions Series - Greet developers.js
+++ Daily Solutions/9-8-2022/7 kyu Coding Meetup #2 - Higher-Order Functions Series - Greet developers.js
@@ -1,4 +1,4 @@
-// 7 kyu Coding Meetup #2 - Higher-Order Functions Series - Greet develofunction greetDevelopers(list) {
+// 7 kyu Coding Meetup #2 - Higher-Order Functions Series - Greet developers
 function greetDevelopers(list) {
   list.forEach((obj) => {
     obj.greeting = `Hi ${obj.firstName}, what do you like the most about ${obj.language}?`;
