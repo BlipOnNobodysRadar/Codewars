c06c634275b92ec51c086519dc8c920499cc7fd6
Ryan Lowe <somebody480@gmail.com>
2022-10-06 17:36:00 -0400

    7 kyu shorter concat [reverse longer]

--- Daily Solutions/10-6-2022/7 kyu shorter concat [reverse longer].js
+++ Daily Solutions/10-6-2022/7 kyu shorter concat [reverse longer].js
@@ -4,6 +4,7 @@ function shorter_reverse_longer(a, b) {
     ? b + a.split("").reverse().join("") + b
     : a + b.split("").reverse().join("") + a;
 }
+// arrow function
 const shorter_reverse_longer = (a, b) =>
   a.length >= b.length
     ? b + a.split("").reverse().join("") + b
