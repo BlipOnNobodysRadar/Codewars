0048d21b5ad26c205b8c648bb790bf6f11de14a2
Ryan Lowe <somebody480@gmail.com>
2022-10-12 14:08:32 -0400

    removed console logs from 7 kyu Broken sequence

--- Daily Solutions/10-12-2022/7 kyu Broken sequence.js
+++ Daily Solutions/10-12-2022/7 kyu Broken sequence.js
@@ -3,7 +3,6 @@ function findMissingNumber(sequence) {
   if (sequence === "") return 0;
   let arr = sequence.split(" ").map((el) => Number(el));
   arr.sort((a, b) => a - b);
-  console.log(arr);
   if (arr[0] !== 1 || arr.includes(NaN)) return 1;
   let last = arr[0];
   for (let i = 1; i < arr.length; i++) {
