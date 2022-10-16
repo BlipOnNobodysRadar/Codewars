7523958595027be2dd1fb9da032322971a239bfb
Ryan Lowe <somebody480@gmail.com>
2022-10-16 12:35:35 -0400

    7 kyu Who's Online optimal solution added

--- Daily Solutions/10-16-2022/7 kyu Who's Online.js
+++ Daily Solutions/10-16-2022/7 kyu Who's Online.js
@@ -17,3 +17,21 @@ const whosOnline = (friends) => {
   });
   return output;
 };
+
+//optimal solution
+/*
+const whosOnline = (friends) => {
+  let output = {};
+  friends.forEach(e => {
+      let status = e.status;
+      if(status === 'online' && e.lastActivity > 10) status = 'away';
+      if(output[status]) {
+          output[status].push(e.username);
+      }
+      else {
+          output[status] = [e.username];
+      }
+  });
+  return output;
+}
+*/
