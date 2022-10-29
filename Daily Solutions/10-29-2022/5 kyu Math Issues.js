cede8cec5f994476fff0b8a18649c6a27bc324de
Ryan Lowe <somebody480@gmail.com>
2022-10-29 14:17:32 -0400

    5 kyu Math Issues

---
+++ Daily Solutions/10-29-2022/5 kyu Math Issues.js
@@ -0,0 +1,21 @@
+// 5 kyu Math Issues
+// convert to string, split on ".", first entry in 2nd arr >= 5 round up
+
+Math.round = function (number) {
+  console.log(number);
+  const splitStr = String(number).split(".");
+  if (splitStr[1]) {
+    return Number(splitStr[1][0]) >= 5 ? Math.ceil(number) : Math.floor(number);
+  }
+  return number;
+};
+
+Math.ceil = function (number) {
+  const splitStr = String(number).split(".");
+  return splitStr[1] ? Number(splitStr[0]) + 1 : Number(splitStr[0]);
+};
+
+Math.floor = function (number) {
+  const splitStr = String(number).split(".");
+  return Number(splitStr[0]);
+};
