b17322c9f0f697b76c45dbbaa864d112fce38c81
Ryan Lowe <somebody480@gmail.com>
2023-01-09 15:12:31 -0500

    7 kyu Dictionary from two lists

---
+++ Daily Solutions/2023/1-6/7 kyu Dictionary from two lists.js
@@ -0,0 +1,15 @@
+// 7 kyu Dictionary from two lists
+function createDict(keys, values) {
+  const obj = {};
+  for (let i = 0; i < keys.length; i++) {
+    obj[keys[i]] = values[i];
+  }
+  for (let key in obj) {
+    if (obj[key] === undefined) {
+      obj[key] = null;
+    }
+  }
+  return obj;
+}
+
+createDict(["a", "b", "c"], [1, 2, 3]);
