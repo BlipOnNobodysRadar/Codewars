4bb567001f70820cb3c3fa6b9b93862eb682b204
Ryan Lowe <somebody480@gmail.com>
2023-01-09 15:53:22 -0500

    6 kyu Array Deep Count -- No Recursion version

--- Daily Solutions/2023/1-9/6 kyu Array Deep Count.js
+++ Daily Solutions/2023/1-9/6 kyu Array Deep Count.js
@@ -9,3 +9,29 @@ function deepCount(a) {
   }
   return count;
 }
+
+// alt, no recursion
+function deepCount(a) {
+  let count = 0;
+  let subArrays = new Array();
+  for (let i = 0; i < a.length; i++) {
+    count++;
+    if (!Array.isArray(a[i])) {
+      continue;
+    } else {
+      subArrays.push(a[i]);
+    }
+  }
+  while (subArrays.length > 0) {
+    currArr = subArrays.pop();
+    for (let j = 0; j < currArr.length; j++) {
+      count++;
+      if (!Array.isArray(currArr[j])) {
+        continue;
+      } else {
+        subArrays.push(currArr[j]);
+      }
+    }
+  }
+  return count;
+}
