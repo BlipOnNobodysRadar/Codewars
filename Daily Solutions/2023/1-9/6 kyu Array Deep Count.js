6bc888c6f449bf78720034aab4a561625ee1a3e1
Ryan Lowe <somebody480@gmail.com>
2023-01-09 15:44:10 -0500

    6 kyu Array Deep Count

---
+++ Daily Solutions/2023/1-9/6 kyu Array Deep Count.js
@@ -0,0 +1,11 @@
+// 6 kyu Array Deep Count
+function deepCount(a) {
+  let count = 0;
+  for (let i = 0; i < a.length; i++) {
+    count++;
+    if (Array.isArray(a[i])) {
+      count += deepCount(a[i]);
+    }
+  }
+  return count;
+}
